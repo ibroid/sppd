3e8235edfb7d209c43161fcaa9b92919fcd5d4a8
ibroid <maulana858@gmail.com>
2025-11-05 16:33:11 +0700

    fix wa api key header

--- application/libraries/Wanotif.php
+++ application/libraries/Wanotif.php
@@ -38,7 +38,9 @@ class Wanotif
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string)
+                'Content-Length: ' . strlen($data_string),
+                'Accept: application/json',
+                'X-Api-Key: ' .  $_ENV["WA_API_TOKEN"]
             ));
 
             $result = curl_exec($ch);
